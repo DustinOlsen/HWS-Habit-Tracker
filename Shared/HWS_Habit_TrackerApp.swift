2fcb964dda177e3be77b61fee22faf2cd995b8a0
Dustin <Olsen>
2021-02-04 11:04:53 -0600

    Initial Commit

---
+++ Shared/HWS_Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Habit_TrackerApp.swift
+//  Shared
+//
+//  Created by Dustin Olsen on 2/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
